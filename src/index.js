9688dcaf1c38b1bf29939cf03e619c4590eca6f0
Juliana Lima <julianalimab88@gmail.com>
2021-08-21 16:06:36 -0300

    organizando pastas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Repositories from './Repositories' ; 
+import Repositories from './Pages/Home/Repositories/Repositories' ; 
 
 ReactDOM.render(
   <React.StrictMode>
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Repositories from './Repositories';
-import Home from './Home';
+import Repositories from './Pages/Home/Repositories/Repositories';
+import Home from './Pages/Home/Home';
 
 export default function Routes () {
   return (
     <BrowserRouter>
     <Switch>
-      <Route path='/' exact component={ Home} />
+      <Route path='/' exact component={Home} />
       <Route path='/repositories' component={Repositories} />
     </Switch>
     </BrowserRouter>
